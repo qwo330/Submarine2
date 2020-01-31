d5dca6b4e5d592d956a918ba5c54752d1d55268a
qwo330 <qwd330@naver.com>
2020-02-01 01:55:59 +0900

    씬 업데이트 및 게임 매니저 코드 추가

--- Submarine/Assets/Scripts/GameManager.cs
+++ Submarine/Assets/Scripts/GameManager.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+public enum GameState
+{
+    Ready,
+    Play,
+    GameOver,
+}
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static GameManager _instance;
+    public static GameManager Instance { get; private set; }
+
+    #region Inspector
+    public SubMarine Player;
+    public GameObject UI_Result;
+    #endregion
+
+    public GameState state;
+    public UnityAction<int> oneSecEvent;
+    public int Time;
+
+    WaitForSeconds oneSec = new WaitForSeconds(1f);
+
+    void Awake()
     {
-        
+        oneSecEvent += HardMore;
+        state = GameState.Ready;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameStart()
     {
-        
+        state = GameState.Play;
+        UI_Result.SetActive(false);
+        Time = 0;
+        StartCoroutine(Timer());
+    }
+
+    public void HardMore(int sec)
+    {
+
+    }
+
+    IEnumerator Timer()
+    {
+        yield return oneSec;
+        Time++;
+        oneSecEvent.Invoke(Time);
+    }
+
+    public void CheckGameOver()
+    {
+        if (Player.HP < 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        state = GameState.GameOver;
+        UI_Result.SetActive(true);
+        StopAllCoroutines();
     }
 }
