144a131a0e3b76b5efaa7d1907bee15ece8ad53e
qwo330 <qwd330@naver.com>
2020-02-01 16:45:28 +0900

    피격 카메라 연출 추가

---
+++ Submarine/Assets/Scripts/UI/UI_RedEffect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class UI_RedEffect : MonoBehaviour
+{
+    public const string key_Warning = "Warning";
+    public const string key_Hit = "Hit";
+
+    [Header("Setting")]
+    int WarningHP = 10;
+
+    [Space(10)]
+    [SerializeField]
+    Animator animator;
+
+    void Start()
+    {
+        GameManager.Instance.HitEvent += HitEffect;
+    }
+
+    public void HitEffect(int hp)
+    {
+        animator.SetTrigger(key_Hit);
+
+        if (hp <= WarningHP)
+            WarningEffect();
+    }
+
+    public void WarningEffect()
+    {
+        animator.SetBool(key_Warning, true);
+    }
+
+    public void WarningEffectOff()
+    {
+        animator.SetBool(key_Warning, false);
+    }
+}
