9e93acbcac4484489a52395d9ab0f9a71f225f0d
qwo330 <qwd330@naver.com>
2020-02-02 07:26:19 +0900

    itemSpawner 추가

---
+++ Submarine/Assets/Scripts/Object/ItemSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public MapType type;
+    GameObject getObject;
+
+    void OnEnable()
+    {
+        LoadObject();
+    }
+
+    void OnDisable()
+    {
+        if (getObject != null)
+            ObjectPool.Instance.ReturnObject(getObject);    
+    }
+
+    void LoadObject()
+    {
+        getObject = ObjectPool.Instance.GetObject(type.ToString());
+        getObject.transform.position = transform.position;
+    }
+}
