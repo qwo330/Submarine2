e2580a94602cae8a927559a46e6f1e6a3a44acc7
qwo330 <qwd330@naver.com>
2020-02-01 12:22:40 +0900

    맵 오브젝트 프리팹 추가

---
+++ Submarine/Assets/Scripts/Object/BaseObstacle.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseObstacle : MonoBehaviour
+{
+    [Header("Setting")]
+    public int Power = 5;
+}
---
+++ Submarine/Assets/Scripts/Object/Map.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : BaseObstacle
+{
+    MapType Type;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameManager.Instance.CreateMap();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Invoke("WaitAndReturn", 3f);
+        }
+    }
+
+    void WaitAndReturn()
+    {
+        ObjectPool.Instance.ReturnObject(gameObject);
+    }
+}
