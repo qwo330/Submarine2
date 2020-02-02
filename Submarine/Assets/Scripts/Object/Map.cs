61242a55f388d922403fc5d0bf365d883be9f6ba
qwo330 <qwd330@naver.com>
2020-02-02 09:10:26 +0900

    맵 트리거 수정

--- Submarine/Assets/Scripts/Object/Map.cs
+++ Submarine/Assets/Scripts/Object/Map.cs
@@ -1,34 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Map : BaseObstacle
 {
-    MapType Type;
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(GameManager.Tag_Player))
         {
+            var player = collision.GetComponent<SubMarine>();
+            player.GetDamage(Power);
             //GameManager.Instance.CreateMap();
         }
     }
 
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag(GameManager.Tag_Player))
-        {
-            CollisionAction();
-        }
-    }
-
-    public override void CollisionAction()
-    {
-        Invoke("WaitAndReturn", 3f);
-    }
-
-    void WaitAndReturn()
-    {
-        ObjectPool.Instance.ReturnObject(gameObject);
-    }
+    public override void CollisionAction() { }
 }
---
+++ Submarine/Assets/Scripts/Object/MapTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(GameManager.Tag_Player))
+        {
+            GameManager.Instance.CreateMap();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(GameManager.Tag_Player))
+        {
+            CollisionAction();
+        }
+    }
+
+    public void CollisionAction()
+    {
+        Invoke("WaitAndReturn", 3f);
+    }
+
+    void WaitAndReturn()
+    {
+        ObjectPool.Instance.ReturnObject(transform.parent.gameObject);
+    }
+}
